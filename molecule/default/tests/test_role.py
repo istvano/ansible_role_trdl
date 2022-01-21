0dec44467b6415d068f58cc0b4c52a4ed7792c6b
Istvan Orban <istvan.orban@gmail.com>
2022-01-21 18:20:51 +0000

    install it to the user's bin folder

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -7,15 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_dir(host):
-    dir = host.file('/usr/local/bin')
+    dir = host.file('/root/bin')
     assert dir.exists
     assert dir.is_directory
-    assert dir.user == 'root'
 
 
 def test_file(host):
-    installed_file = host.file('/usr/local/bin/trdl')
+    installed_file = host.file('/root/bin/trdl')
     assert installed_file.exists
     assert installed_file.is_file
-    assert installed_file.user == 'root'
-    assert installed_file.group == 'root'
